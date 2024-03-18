bd1f446261fb62fe13064eca45d333778beb3a61
Jonathan Hendler <jonathan.hendler@gmail.com>
2024-03-18 04:15:44 -0700

    correctly failing test

--- tests/create_agent_test.rs
+++ tests/create_agent_test.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 #[test]
-fn test_validate_agent_json() {
+fn test_validate_agent_creation() {
     let agent_version = "v1".to_string();
     let header_version = "v1".to_string();
     let mut agent = jacs::agent::Agent::new(&agent_version, &header_version).unwrap();
@@ -17,4 +17,22 @@ fn test_validate_agent_json() {
     };
 
     println!("New Agent Created {} ", agent);
+}
+
+#[test]
+fn test_invalidate_existing_agent() {
+    let agent_version = "v1".to_string();
+    let header_version = "v1".to_string();
+    let mut agent = jacs::agent::Agent::new(&agent_version, &header_version).unwrap();
+    let json_data = fs::read_to_string("examples/agents/agent-two.json").expect("REASON");
+    let result = agent.create_agent_and_use(&json_data, false, None);
+
+    let _ = match result {
+        Ok(_) => Ok(result),
+        Err(error) => Err({
+            println!("New Agent Not {} ", agent);
+            println!("{}", error);
+            assert!(true);
+        }),
+    };
 }
