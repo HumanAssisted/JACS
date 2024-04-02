3d5dd7d737ac0f4220c8c3f246e56faa1b72f07d
Jonathan Hendler <jonathan.hendler@gmail.com>
2024-04-02 06:33:32 -0700

    enc, dec

--- src/crypt/aes_encrypt.rs
+++ src/crypt/aes_encrypt.rs
@@ -1,84 +1,78 @@
-use ring::aead::BoundKey;
-use ring::aead::UnboundKey;
-use ring::rand::SecureRandom;
-use ring::{aead, pbkdf2, rand};
-use std::num::NonZeroU32;
+use aes_gcm::AeadCore;
+use aes_gcm::{
+    aead::{Aead, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce,
+};
+use rand::{thread_rng, Rng};
+use sha2::{Digest, Sha256};
+use std::str;
 
-static PBKDF2_ALG: pbkdf2::Algorithm = pbkdf2::PBKDF2_HMAC_SHA256;
-const PBKDF2_ITERATIONS: NonZeroU32 = NonZeroU32::new(100_000).unwrap();
-const PBKDF2_SALT_LEN: usize = 16;
-const KEY_LEN: usize = 32;
-const TAG_LEN: usize = 16;
-
-// Encrypts a private key using a password
+// Encrypt a private key with a password
 pub fn encrypt_private_key(
     password: &str,
     private_key: &[u8],
-) -> Result<Vec<u8>, ring::error::Unspecified> {
-    let rng = rand::SystemRandom::new();
-    let mut salt = [0; PBKDF2_SALT_LEN];
-    rng.fill(&mut salt)?;
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    // Generate a random salt
+    let mut salt = [0u8; 16];
+    thread_rng().fill(&mut salt[..]);
 
-    let mut key = [0u8; KEY_LEN];
-    pbkdf2::derive(
-        PBKDF2_ALG,
-        PBKDF2_ITERATIONS,
-        &salt,
-        password.as_bytes(),
-        &mut key,
-    );
+    // Derive key using PBKDF2 with SHA-256
+    let mut key = [0u8; 32];
+    let mut hasher = Sha256::new();
+    hasher.update(password.as_bytes());
+    hasher.update(&salt);
+    let hash = hasher.finalize();
+    key.copy_from_slice(&hash[..32]);
 
-    let mut nonce = [0u8; 12];
-    rng.fill(&mut nonce)?;
+    // Create cipher instance
+    let key = Key::<Aes256Gcm>::from_slice(&key);
+    let cipher = Aes256Gcm::new(key);
 
-    let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &key)?;
-    let mut sealing_key = aead::SealingKey::new(unbound_key, &nonce)?;
+    // Generate a random nonce
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
-    let aad = aead::Aad::empty();
+    // Encrypt private key
+    let encrypted_data = cipher
+        .encrypt(&nonce, private_key)
+        .expect("encryption failure!");
 
-    let mut in_out = private_key.to_vec();
-    in_out.extend_from_slice(&[0u8; TAG_LEN]);
+    // Combine the salt, nonce, and encrypted data into one Vec to return
+    let mut encrypted_key_with_salt_and_nonce = salt.to_vec();
+    encrypted_key_with_salt_and_nonce.extend_from_slice(nonce.as_slice());
+    encrypted_key_with_salt_and_nonce.extend_from_slice(&encrypted_data);
 
-    let tag = sealing_key.seal_in_place_separate_tag(aad, &mut in_out)?;
-
-    let mut encrypted_data = Vec::with_capacity(salt.len() + nonce.len() + in_out.len() + TAG_LEN);
-    encrypted_data.extend_from_slice(&salt);
-    encrypted_data.extend_from_slice(&nonce);
-    encrypted_data.extend_from_slice(&in_out);
-    encrypted_data.extend_from_slice(tag.as_ref());
-
-    Ok(encrypted_data)
+    Ok(encrypted_key_with_salt_and_nonce)
 }
 
-// Decrypts an encrypted private key using a password
+// Decrypt the private key with the password
 pub fn decrypt_private_key(
     password: &str,
-    encrypted_data: &[u8],
-) -> Result<Vec<u8>, ring::error::Unspecified> {
-    if encrypted_data.len() < PBKDF2_SALT_LEN + KEY_LEN {
-        return Err(ring::error::Unspecified);
+    encrypted_key_with_salt_and_nonce: &[u8],
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    if encrypted_key_with_salt_and_nonce.len() < 16 + 12 {
+        return Err("encrypted data is too short".into());
     }
 
-    let (salt, rest) = encrypted_data.split_at(PBKDF2_SALT_LEN);
-    let (nonce, rest) = rest.split_at(12);
-    let (encrypted_private_key, tag) = rest.split_at(rest.len() - TAG_LEN);
+    // Split the data into salt, nonce, and encrypted key
+    let (salt, rest) = encrypted_key_with_salt_and_nonce.split_at(16);
+    let (nonce, encrypted_data) = rest.split_at(12);
 
-    let mut key = [0u8; KEY_LEN];
-    pbkdf2::derive(
-        PBKDF2_ALG,
-        PBKDF2_ITERATIONS,
-        salt,
-        password.as_bytes(),
-        &mut key,
-    );
+    // Derive key using PBKDF2 with SHA-256
+    let mut key = [0u8; 32];
+    let mut hasher = Sha256::new();
+    hasher.update(password.as_bytes());
+    hasher.update(salt);
+    let hash = hasher.finalize();
+    key.copy_from_slice(&hash[..32]);
 
-    let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &key)?;
-    let mut opening_key = aead::OpeningKey::new(unbound_key, &nonce)?;
+    // Create cipher instance
+    let key = Key::<Aes256Gcm>::from_slice(&key);
+    let cipher = Aes256Gcm::new(key);
 
-    let aad = aead::Aad::empty();
+    // Decrypt private key
+    let decrypted_data = cipher
+        .decrypt(&Nonce::from_slice(nonce), encrypted_data)
+        .expect("decryption failure!");
 
-    let mut in_out = encrypted_private_key.to_vec();
-    let decrypted_data = opening_key.open_in_place(aad, &mut in_out, tag)?;
-
-    Ok(decrypted_data.to_vec())
+    Ok(decrypted_data)
 }
