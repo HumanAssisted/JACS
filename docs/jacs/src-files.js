59fb727a9dd06feb169bc5e08f0a695b18c32101
Jonathan Hendler <jonathan.hendler@gmail.com>
2024-04-09 01:47:33 -0700

    testing new doc

---
+++ docs/jacs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["jacs",["",[["agent",[],["boilerplate.rs","document.rs","loaders.rs","mod.rs"]],["config",[],["mod.rs"]],["crypt",[],["aes_encrypt.rs","hash.rs","mod.rs","pq.rs","ringwrapper.rs","rsawrapper.rs"]],["schema",[],["mod.rs","signature.rs","utils.rs"]]],["lib.rs"]]]\
+]'));
+createSrcSidebar();
