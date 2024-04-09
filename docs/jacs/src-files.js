4bf4ea1907e6bff8d02cd836caf0b60331aef51d
Jonathan Hendler <jonathan.hendler@gmail.com>
2024-04-09 01:53:16 -0700

    get css

--- docs/jacs/src-files.js
+++
@@ -1,4 +0,0 @@
-var srcIndex = new Map(JSON.parse('[\
-["jacs",["",[["agent",[],["boilerplate.rs","document.rs","loaders.rs","mod.rs"]],["config",[],["mod.rs"]],["crypt",[],["aes_encrypt.rs","hash.rs","mod.rs","pq.rs","ringwrapper.rs","rsawrapper.rs"]],["schema",[],["mod.rs","signature.rs","utils.rs"]]],["lib.rs"]]]\
-]'));
-createSrcSidebar();
