93168f2e1b74b70b139db43909b6af6865e7a28f
Ubuntu <ubuntu@ip-10-240-139-167.us-west-2.compute.internal>
2024-05-02 13:27:58 +0000

    Fix schema loading from local paths in load_custom_schemas

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -632,9 +632,7 @@ impl Agent {
             } else {
                 // Load schema from local file
                 println!("loading custom schema {}", path);
-                let base_path = env::current_dir().unwrap(); // Get the current working directory
-                let schema_path = base_path.join(path); // Create an absolute path from the base path and the relative path
-                let schema_json = std::fs::read_to_string(schema_path).unwrap();
+                let schema_json = std::fs::read_to_string(path).unwrap();
                 let schema_value: Value = serde_json::from_str(&schema_json).unwrap();
                 JSONSchema::options()
                     .with_draft(Draft::Draft7)
