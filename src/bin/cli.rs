aae98264f28c40981c027c395cae4533b57aba08
Ubuntu <ubuntu@ip-10-240-149-111.us-west-2.compute.internal>
2024-05-15 10:49:18 +0000

    Fix argument types for Agent::new function call

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -592,13 +592,8 @@ fn main() {
                     "https://hai.ai/schemas/header/v1/header.schema.json".to_string();
                 let agent_schema_url =
                     "https://hai.ai/schemas/agent/v1/agent.schema.json".to_string();
-                let mut agent = Agent::new(
-                    "v1".to_string(),
-                    "v1".to_string(),
-                    header_schema_url,
-                    agent_schema_url,
-                )
-                .expect("Failed to create agent");
+                let mut agent = Agent::new("v1", "v1", header_schema_url, agent_schema_url)
+                    .expect("Failed to create agent");
                 agent
                     .create_agent_and_load(&agentstring)
                     .expect("Failed to create and load agent from provided JSON data");
