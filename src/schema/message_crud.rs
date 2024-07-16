78a784370ca337cd8ff9d17d7d0c9e29b7b9954d
Jonathan Hendler <jonathan.hendler@gmail.com>
2024-07-16 17:44:08 +0200

    create message test

--- src/schema/message_crud.rs
+++ src/schema/message_crud.rs
@@ -1,4 +1,4 @@
-use crate::agent::document::DocumentTraits;
+use crate::agent::document::{DocumentTraits, JACSDocument};
 use crate::Agent;
 use chrono::Utc;
 use serde_json::{json, Value};
@@ -6,22 +6,17 @@ use std::error::Error;
 use uuid::Uuid;
 
 /// Creates a minimal message with required fields.
-/// # Arguments
-///
-/// * `content` - The content of the message.
-/// * task_id - id of the task this is about
-/// * attachments - list of filepaths
-/// # Returns
-///
+/// message are immutable and signed so theres no update method
 /// A `serde_json::Value` representing the created message.
-pub fn create_minimal_message(
+pub fn create_message(
     agent: &mut Agent,
     content: Value,
-    task_id: String,
-    // _to: Option<Vec<String>>,
+    to: Vec<String>,
+    from: Vec<String>,
+    // todo add optional taskid/thread context
     attachments: Option<Vec<String>>,
     embed: Option<bool>,
-) -> Result<Value, Box<dyn Error>> {
+) -> Result<JACSDocument, Box<dyn Error>> {
     let datetime = Utc::now();
     let schema = "https://hai.ai/schemas/message/v1/message.schema.json";
 
@@ -29,32 +24,12 @@ pub fn create_minimal_message(
         "$schema": schema,
         "datetime": datetime.to_rfc3339(),
         "content": content,
-        "taskID": task_id
+        "to": to,
+        "from": from
     });
-
-    // optionally add attachements
-    if let Some(attachment_list) = attachments {
-        let mut files_array: Vec<Value> = Vec::new();
-
-        // Iterate over each attachment
-        for attachment_path in attachment_list {
-            let final_embed = embed.unwrap_or(false);
-            let file_json = agent
-                .create_file_json(&attachment_path, final_embed)
-                .unwrap();
-
-            // Add the file JSON to the files array
-            files_array.push(file_json);
-        }
-
-        // Create a new "files" field in the document
-        // let instance_map = message.as_object_mut().unwrap();
-        message["attachments"] = Value::Array(files_array);
-    }
-    // sign
-    message["signature"] = agent.signing_procedure(&message, None, &"signature".to_string())?;
-
-    message["id"] = json!(Uuid::new_v4().to_string());
-
+    // convert to json string
+    let message_str = serde_json::to_string(&message)?;
+    // create doc with schema checking and attachments using standard JacsDocument
+    let message: JACSDocument = agent.create_document_and_load(&message_str, attachments, embed)?;
     Ok(message)
 }
