39d9c576466113cb3bd89478a6925ea75968f351
Jonathan Hendler <jonathan.hendler@gmail.com>
2026-02-07 17:45:24 -0800

    go build

--- jacsgo/jacs.go
+++ jacsgo/jacs.go
@@ -7,6 +7,7 @@ package jacs
 
 #include <stdlib.h>
 #include <stdint.h>
+#include "jacs_cgo.h"
 
 // JacsAgent handle API - Recommended for concurrent usage
 typedef void* JacsAgentHandle;
@@ -48,7 +49,6 @@ char* jacs_verify_response(const char* document_string);
 char* jacs_verify_response_with_agent_id(const char* document_string, char** agent_id_out);
 int jacs_verify_signature(const char* document_string, const char* signature_field);
 char* jacs_verify_document_standalone(const char* signed_document, const char* key_resolution, const char* data_directory, const char* key_directory);
-char* jacs_audit(const char* config_path, int recent_n);
 char* jacs_generate_verify_link(const char* document, const char* base_url);
 */
 import "C"
