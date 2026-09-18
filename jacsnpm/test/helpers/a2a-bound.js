bf322e95e1b06866239d883e3315310decf104d3
Jonathan Hendler <jonathan.hendler@gmail.com>
2026-09-17 17:23:33 -0700

    Refresh W3C and Node verification capability fixtures
    Local checkpoint 4808274185d492eb4dfe44830882ae3b8b3961ef

--- jacsnpm/test/helpers/a2a-bound.js
+++ jacsnpm/test/helpers/a2a-bound.js
@@ -47,7 +47,7 @@ function boundWellKnownPairs(options = {}) {
       capabilities: {
         signing: true,
         verification: true,
-        verificationAlgorithms: ['ring-Ed25519', 'pq2025'],
+        verificationAlgorithms: ['ring-Ed25519', 'pq2025', 'es256'],
         postQuantum: keyAlgorithm === 'pq2025',
       },
       schemas: {
@@ -66,7 +66,7 @@ function boundWellKnownPairs(options = {}) {
       capabilities: {
         documentSigning: { signingAlgorithm: keyAlgorithm },
         documentVerification: {
-          algorithms: ['ring-Ed25519', 'pq2025'],
+          algorithms: ['ring-Ed25519', 'pq2025', 'es256'],
           offlineCapable: true,
         },
         postQuantumCrypto: { algorithms: ['pq2025'] },
